aaf510a458b43dfcd4a57490225b379e8a3249e9
EduardToma <eduard.daniel.toma@adicomsoft.ro>
2021-03-11 09:10:37 +0200

    test

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,7 +16,21 @@ export class AboutComponent implements OnInit {
 
     console.log('a', );
 
+    const http$ = createHttpObservable('/api/courses');
 
+    const courses$ = http$
+      // allow to chain operations
+      .pipe(
+        map(res => Object.values(res['payload'])), // transform into an object every res
+        map(e => e['id'] = 333) // trnsform into an object every res
+      )
+
+      courses$.subscribe(
+      courses => console.log(courses),
+      // () => {}, // empty documentation or callback
+      noop,
+      () => console.log('completed')
+    );
 
   }
 }
